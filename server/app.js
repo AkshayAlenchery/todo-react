f3a81bb0bc88e0c59cce9f0e36d02f13e5a6b70f
Akshay Alenchery <akshayalenchery@gmail.com>
2020-02-13 08:55:31 +0530

    Added mongodb connections

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
+
 const { connectDB } = require('./db')
+const routes = require('./routes/index.routes')
+
 const app = express()
 const port = process.env.APP_PORT || 8000
 
+connectDB()
 app.use(express.json())
 app.use(cors())
-connectDB()
+app.use('/api/v3.0/', routes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)

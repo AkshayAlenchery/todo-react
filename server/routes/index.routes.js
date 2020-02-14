dc7c360350ffd53659bebc485928267df09b17c0
Akshay Alenchery <akshayalenchery@gmail.com>
2020-02-14 12:33:58 +0530

    Added task route to routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const Router = express.Router()
 
 const listRoutes = require('./list.routes')
+const taskRoutes = require('./task.routes')
 
 Router.use('/list', listRoutes)
+
+Router.use('/task', taskRoutes)
 
 module.exports = Router

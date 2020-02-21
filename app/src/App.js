9944b8a17171e720b0d676ad6f639a1a1c834727
Akshay Alenchery <akshayalenchery@gmail.com>
2020-02-21 16:39:58 +0530

    Added custom messages

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import List from './components/list/lists'
 import Task from './components/tasks/tasks'
 import NotFound from './components/404/NotFound'
+import './App.css'
 
 function App() {
+  const [message, setMsg] = useState({ type: null, message: null })
+
+  const setMessage = msg => {
+    setMsg(msg)
+    setTimeout(() => {
+      setMsg({ type: null, message: null })
+    }, 4000)
+  }
   return (
     <div className='app'>
-      <h1>Todo App</h1>
+      <h1 id='logo'>Todo App</h1>
+      <div>
+        {message.message ? (
+          <p className={message.type === 'error' ? 'err-msg' : 'scs-msg'}>{message.message}</p>
+        ) : (
+          ''
+        )}
+      </div>
       <Router>
         <Switch>
           <Route path='/' exact>
-            <List />
+            <List showMsg={msg => setMessage(msg)} />
           </Route>
           <Route path='/tasks/:listId' exact>
-            <Task />
+            <Task showMsg={msg => setMessage(msg)} />
           </Route>
           <Route path='*'>
             <NotFound />

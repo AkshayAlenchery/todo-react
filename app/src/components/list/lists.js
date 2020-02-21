aa91eae583d82ddeae50041825205f170b62e711
Akshay Alenchery <akshayalenchery@gmail.com>
2020-02-21 10:45:11 +0530

    Removed router

--- app/src/components/list/lists.js
+++ app/src/components/list/lists.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import Axios from 'axios'

3ad1f95f243bcb664a59f0ad0719e8f06534fecd
Akshay Alenchery <akshayalenchery@gmail.com>
2020-04-06 20:26:00 +0530

    Production code

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const path = require('path')
 
 dotenv.config()
 const { connectDB } = require('./db')
@@ -13,7 +14,12 @@ connectDB()
 app.use(express.json())
 app.use(cors())
 app.use('/api/v3.0/', routes)
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
